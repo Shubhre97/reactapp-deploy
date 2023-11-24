55926bba935bc79cc9e59517b3820c4eb56cce99
Shubhre97 <nitmshubham@gmail.com>
2023-11-24 15:42:05 -0500

    jenkins file added45

--- src/components/UI/ErrorAlert.js
+++ src/components/UI/ErrorAlert.js
@@ -5,7 +5,7 @@ import './ErrorAlert.css';
 function ErrorAlert(props) {
   return (
     <section className='error-alert'>
-      <h2>Error!!!!</h2>
+      <h2>Error alaa!!!!</h2>
       <p>{props.errorText}</p>
     </section>
   );
